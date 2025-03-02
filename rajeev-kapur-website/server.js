868de87b2ff34b2573b7f810e6f03b6f697aabea
rkapur68 <rkapur.2101@gmail.com>
2025-03-02 07:58:23 -0800

    Ver 0.22

--- rajeev-kapur-website/server.js
+++ rajeev-kapur-website/server.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 3000;
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+// Add Cache-Control headers
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  next();
+});
+
 // Add the Content-Security-Policy header (before app.listen)
 app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy', "script-src 'self' 'unsafe-eval';");
